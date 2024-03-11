35a4d75fd9f2201b125d1d99e1eb93fb0cac5afd
Wafflesjohn <112663401+Wafflesjohn@users.noreply.github.com>
2024-03-11 15:08:02 -0400

    scr/1

---
+++ client/src/components/productCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { Product } from "@/model/products";
+
+const pages =defineProps<{
+    products: Product
+}>();
+}
+</script>
+
+<template>
+    
+        <div class="card" >
+            <div class="card-image">
+                <img :src="product.thumbnail" :alt="product.title" />
+            </div>
+            <div class="card-content">
+                <p class="price">${{ product.price }}</p>
+                <h3>{{ product.title }}</h3>
+                <i>{{ product.brand }}</i>
+                <p>{{ product.description }}</p>
+                <button @click="addToCart(product)" class="button is-primary">Add to Cart</button>
+            </div>
+        </div>
+    
+</template>
